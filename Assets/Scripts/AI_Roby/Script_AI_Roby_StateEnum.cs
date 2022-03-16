1ff8c2979c8cd22d8e54bc729bf512c191fe87dc
Antonio Troiano <antonio.troiano2304@gmail.com>
2022-03-17 00:27:39 +0100

    Roby AI start

--- Assets/Scripts/AI_Roby/Script_AI_Roby_StateEnum.cs
+++ Assets/Scripts/AI_Roby/Script_AI_Roby_StateEnum.cs
@@ -1,8 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 public class Script_AI_Roby_StateEnum : MonoBehaviour
 {
+    private Animator roby_Animator;
+    private GameObject mai_Player;
+    private NavMeshAgent agent;
+
+    private float distanceFromMai;
+    private float maiPlayerNearZone;
+    private bool patrolling = false;
+    private void Awake()
+    {
+        mai_Player = GameObject.Find("Mai_Player");
+        maiPlayerNearZone = 5;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Start()
+    {
+    }
+    private void Update()
+    {
+        if (patrolling)
+        {
+            Patrolling();
+        }
+        else FollowPlayer();
+    }
+    private void FollowPlayer()
+    {
+        distanceFromMai = Vector3.Distance(transform.position, mai_Player.transform.position);
+        if (distanceFromMai > maiPlayerNearZone)
+        {
+            patrolling = false;
+            agent.SetDestination(mai_Player.transform.position);
+            //transform.position = Vector3.Lerp(transform.position, mai_Player.transform.position, Time.deltaTime);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+            patrolling = true;
+            //agent.SetDestination((new Vector3(Random.insideUnitCircle.x, Random.insideUnitCircle.y, 0)*2) + mai_Player.transform.position);
+        }
+    }
+    private void Patrolling()
+    {
+        if (agent.pathStatus == NavMeshPathStatus.PathComplete)
+            agent.SetDestination(mai_Player.transform.position*Random.Range(-2,2));
+    }
 }
--- Assets/Scripts/AI_Roby/Script_Ai_Roby_State.cs
+++ Assets/Scripts/AI_Roby/Script_Ai_Roby_State.cs
@@ -3,15 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-abstract class Script_Ai_Roby_State : MonoBehaviour
+public class Script_Ai_Roby_State : MonoBehaviour
 {
-    virtual public void OnEnter() { }
-    virtual public void OnExit() { }
 
-    protected StateMachine stateMachine;
-    
-    protected void SetStateMachine(StateMachine stateMachine)
-    {
-        this.stateMachine = stateMachine;
-    }
 }
+
