b4d6237a92f6967bd8b906d8763223b8bf422274
Davide Colella <44302534+wildheathen@users.noreply.github.com>
2022-03-29 23:59:01 +0200

    Fixed button navigation

--- Assets/Scripts/Menu/GraphicsMenu.cs
+++ Assets/Scripts/Menu/GraphicsMenu.cs
@@ -4,32 +4,40 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using System.Linq;
+using System.Management;
+using System.ComponentModel;
 using UnityEngine.Rendering;
 using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
 using Toggle = UnityEngine.UI.Toggle;
 
 public class GraphicsMenu : MonoBehaviour
 {
+    public Slider Framerate;
     public Toggle VSync, HDR;
     public TMP_Dropdown resDropdown, DisplayMode, Shadows, Displays;
     private Resolution[] reversedRes;
     void Start()
     {
-        foreach (var display in Display.displays)
-        {
-            Debug.Log(display.ToString());       
-        }
+        // foreach (var display in Display.displays)
+        // {
+        //     Debug.Log(display.ToString());       
+        // }
+        
         
         //Preparo la Dropdown per le risoluzioni
         #region Risoluzioni
-
+        
         reversedRes = Screen.resolutions.Reverse().ToArray();
+       /* reversedRes.Select(resolution => new Resolution {width = resolution.width, height = resolution.height})
+            .Distinct();*/
         //Rigiro la lista delle risoluzioni altrimenti me le propone dalla più bassa
         foreach (var resolution in reversedRes)
         {
             resDropdown.options.Add((new TMP_Dropdown.OptionData() {text=resolution.ToString()}));
         }
-
+        resDropdown.itemText.text = Screen.currentResolution.ToString();
+        resDropdown.RefreshShownValue();
         #endregion
 
         //Preparo la Dropdown per le ombre
@@ -58,12 +66,17 @@ public class GraphicsMenu : MonoBehaviour
         }
 
         #endregion
-
+        //Mi assicuro d'impostare il vsync, che ci sia o meno
+        OnVSyncChange();
+        
+        
+        
     }
 
     public void SetResolution()
     {
         Screen.SetResolution(reversedRes[resDropdown.value].width, reversedRes[resDropdown.value].height, Screen.fullScreen);
+        
     }
 
     public void OnVSyncChange()
@@ -71,14 +84,31 @@ public class GraphicsMenu : MonoBehaviour
         if (VSync.isOn)
         {
             QualitySettings.vSyncCount = 1;
-            Application.targetFrameRate = -1; //Default Framerate
+            //Application.targetFrameRate = -1; //Default Framerate
             VSync.transform.GetChild(1).GetComponent<TMP_Text>().text = "On";
+            //Sincronizzo con il framerate del monitor?
+            
+            Framerate.interactable = false;
+            Framerate.transform.GetChild(0).GetComponent<TMP_Text>().text = String.Empty;;
         }
         else
         {
-            Application.targetFrameRate = 0; //Don't Sync
+            QualitySettings.vSyncCount = 0;
             VSync.transform.GetChild(1).GetComponent<TMP_Text>().text = "Off";
+            
+            Framerate.interactable = true;
+            Framerate.transform.GetChild(0).GetComponent<TMP_Text>().text = ((int)Framerate.value).ToString(); 
+            //richiamo il metodo per settare il framerate altrimenti schizza alle stelle
+            OnFramerateChange();
         }
+    }
+
+    public void OnFramerateChange()
+    {
+        Framerate.transform.GetChild(0).GetComponent<TMP_Text>().text = Framerate.value.ToString();
+        Application.targetFrameRate = (int)Framerate.value;
+        //Per sicurezza tolgo il vsync
+        QualitySettings.vSyncCount = 0;
     }
     
     public void OnDisplayModeChange()
