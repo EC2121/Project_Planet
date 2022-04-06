38caf55dba7b677ce416a0f418127f1f301e84e2
Antonio Troiano <antonio.troiano2304@gmail.com>
2022-04-06 14:30:08 +0200

    Commit_Fix_Chest

--- Assets/Scripts/Interactable/Script_Chest.cs
+++ Assets/Scripts/Interactable/Script_Chest.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 using System;
 using UnityEngine;
 using UnityTemplateProjects.Saves_Scripts;
@@ -11,23 +6,61 @@ public class Script_Chest : MonoBehaviour
 {
     public GameObject Box_GuiInteractWrite;
     public Transform Player_Chest;
-    private Vector3 OldPos;
 
-    private SphereCollider chest_InteractableCollider;
+    private bool box_IsTaken;
+    private BoxCollider box_Collider;
 
-    public bool TakeMe;
     private void OnEnable()
     {
-        Player_State_Machine.takeTheBox.AddListener(() => TakeMe = !TakeMe);
+        Player_State_Machine.takeTheBox.AddListener(() => box_IsTaken = !box_IsTaken);
         SaveSystem.OnSave += SaveSystemOnOnSave;
         SaveSystem.OnLoad += SaveSystemOnOnLoad;
+    }
+
+    private void OnDisable()
+    {
+        Player_State_Machine.takeTheBox.RemoveListener(() => box_IsTaken = !box_IsTaken);
+
+        SaveSystem.OnSave -= SaveSystemOnOnSave;
+        SaveSystem.OnLoad -= SaveSystemOnOnLoad;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            Box_GuiInteractWrite.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            Box_GuiInteractWrite.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && !Box_GuiInteractWrite.activeInHierarchy) Box_GuiInteractWrite.SetActive(true);
+    }
+
+    private void Start()
+    {
+        Box_GuiInteractWrite.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Box_GuiInteractWrite.activeInHierarchy)
+            Box_GuiInteractWrite.transform.position = Camera.main.WorldToScreenPoint(box_Collider.bounds.center + ( Vector3.up * 0.5f ));
+
+        if (box_IsTaken)
+            OnInteraction();
     }
 
     private void SaveSystemOnOnLoad(object sender, EventArgs e)
     {
         GameData data = SaveSystem.LoadPlayer(true);
         transform.position = new Vector3(data.CratePosition[0], data.CratePosition[1], data.CratePosition[2]);
-        transform.rotation = new Quaternion(data.CrateRotation[0], data.CrateRotation[1], 
+        transform.rotation = new Quaternion(data.CrateRotation[0], data.CrateRotation[1],
                                             data.CrateRotation[2], data.CrateRotation[3]);
     }
 
@@ -36,58 +69,9 @@ public class Script_Chest : MonoBehaviour
         SaveSystem.SaveData(gameObject, true);
     }
 
-    private void OnDisable()
-    {
-        Player_State_Machine.takeTheBox.RemoveListener(() => TakeMe = !TakeMe);
-        
-        SaveSystem.OnSave -= SaveSystemOnOnSave;
-        SaveSystem.OnLoad -= SaveSystemOnOnLoad;
-    }
     private void OnInteraction()
     {
-       Box_GuiInteractWrite.SetActive(false);
-       OldPos = transform.position;
-       transform.position = Player_Chest.position;
-       transform.rotation = Player_Chest.rotation;
-    }
-
-    private void OnDetach()
-    {
-        gameObject.GetComponent<Rigidbody>().isKinematic = false;
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-            Box_GuiInteractWrite.SetActive(true);
-
-    }
-    private void OnTriggerExit(Collider other)
-    {
         Box_GuiInteractWrite.SetActive(false);
-    }
-
-    private void Awake()
-    {
-        chest_InteractableCollider = GetComponent<SphereCollider>();
-    }
-    void Start()
-    {
-        Box_GuiInteractWrite.SetActive(false);
-    }
-
-    void Update()
-    {
-        if (Box_GuiInteractWrite.activeInHierarchy)
-            Box_GuiInteractWrite.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 0.6f, 0));
-
-        if (TakeMe)
-        {
-            OnInteraction();
-        }
-
-        if (!TakeMe)
-        {
-            OnDetach();
-        }
+        transform.SetPositionAndRotation(Player_Chest.position, Player_Chest.rotation);
     }
 }
