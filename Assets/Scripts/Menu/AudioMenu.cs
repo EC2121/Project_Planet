6f4993b7a383d0181e7cfae9e61577f1765c5b20
Davide Colella <44302534+wildheathen@users.noreply.github.com>
2022-04-11 00:58:04 +0200

    Changes to Game Menu prefab

---
+++ Assets/Scripts/Menu/AudioMenu.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityTemplateProjects.Menu
+{
+    public class AudioMenu : MonoBehaviour
+    {
+        public Slider SFX, Music;
+    
+        private FMOD.Studio.Bus music;
+        private FMOD.Studio.Bus sfx;
+
+        private void Awake()
+        {
+            try
+            {
+                music = FMODUnity.RuntimeManager.GetBus("bus:/Music");
+                sfx = FMODUnity.RuntimeManager.GetBus("bus:/SFX");
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.ToString());
+            }
+            
+        }
+
+        private void Start()
+        {
+            float tempAudio;
+            try
+            {
+                sfx.getVolume(out tempAudio);
+                SFX.value = (int) tempAudio;
+                
+                music.getVolume(out tempAudio);
+                Music.value = (int) tempAudio;
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.ToString());
+            }
+        }
+
+        public void OnSFXValueChange(float value)
+        {
+            sfx.setVolume(value);
+        }
+
+        public void OnMusicValueChanged(float value)
+        {
+            music.setVolume(value);
+        }
+    }
+}
