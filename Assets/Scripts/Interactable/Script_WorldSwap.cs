688e8013bec1d2074140a40855d52e5c03866fd9
Antonio Troiano <antonio.troiano2304@gmail.com>
2022-04-19 02:13:44 +0200

    Commit_ test Scene
    Alien Test Scene 1
    Alien Test Scene 2
    ASync scene load

--- Assets/Scripts/Interactable/Script_WorldSwap.cs
+++ Assets/Scripts/Interactable/Script_WorldSwap.cs
@@ -1,15 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Script_WorldSwap : MonoBehaviour
 {
     public GameObject ActiveGameOBj;
     public GameObject DeactiveGameOBj;
+    float timer = 6;
+    bool endProve = false;
+    AsyncOperation async;
+
+    private IEnumerator LoadAsyncProcess()
+    {
+        async = SceneManager.LoadSceneAsync("Gameplay_Scene");
+        async.allowSceneActivation = false;
+        while (!async.isDone)
+        {
+            Debug.Log("Loading progress: " + ( async.progress * 100 ) + "%");
+
+            if (async.progress >= 0.9f)
+            {
+                Debug.Log("EndAsync");
+            }
+
+            yield return null;
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LoadAsyncProcess());
+    }
+
+    private void Update()
+    {
+        if (endProve)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                async.allowSceneActivation = true;
+            }
+        }
+    }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
         ActiveGameOBj.SetActive(true);
         DeactiveGameOBj.SetActive(false);
+        endProve = true;
     }
 }
