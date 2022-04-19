2b4a436e323a298cc5a6d37da5397dc072e5ebc4
EC2121 <eliacantinipp@gmail.com>
2022-04-19 17:13:10 +0200

    BreakableWalls created

---
+++ Assets/BreakableObject.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BreakableObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public List<GameObject> FragmentedObj;
+    public Transform ForceOrigin;
+    public float ExplosionRadius;
+    public float ExplosionForce;
+    public float UpwardsModifier;
+    private bool CanExplode;
+    [SerializeField] public LayerMask layerMask;
+    [SerializeField] public ForceMode forceMode;
+    public GameObject OcclusionPortal;
+    void Start()
+    {
+        CanExplode = true;
+        foreach (var item in FragmentedObj)
+        {
+            item.SetActive(false);
+        }
+       
+    }
+
+
+    public void OnFragment()
+    {
+        if (OcclusionPortal != null)
+        {
+            OcclusionPortal.GetComponent<OcclusionPortal>().open = true;
+        }
+
+
+        this.gameObject.SetActive(false);
+        foreach (var item in FragmentedObj)
+        {
+            item.SetActive(true);
+        }
+        Collider[] colliders = Physics.OverlapSphere(ForceOrigin.position, ExplosionRadius, layerMask);
+
+        foreach (var item in colliders)
+        {
+            item.GetComponent<Rigidbody>().AddExplosionForce(ExplosionForce,ForceOrigin.position,ExplosionRadius,UpwardsModifier,forceMode);
+        }
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Keyboard.current.yKey.wasPressedThisFrame && CanExplode)
+        {
+            CanExplode = false;
+            OnFragment();
+        }
+    }
+}
